b380a69d01b00907f627add41667a573464c851f
Alberto <xlock.cervera.alberto@gmail.com>
2021-06-09 10:58:21 +0200

    subir

---
+++ src/calculadora/calc_Test.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class calc_Test {
+	private String commando;
+	@Test
+	public void resultado() {
+		double numero;
+		int j;
+		assertEquals(commando,'%');
+	}
+
+}
